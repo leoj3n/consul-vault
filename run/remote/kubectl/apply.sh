8039f4a4e264cb595b8ea946297dc107db07933a
Joel Kuzmarski <leoj3n@gmail.com>
2019-06-13 02:38:43 -0500

    Use loadbalancer to expose vault

--- run/remote/kubectl/apply.sh
+++ run/remote/kubectl/apply.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env zsh
 
+if [[ -z "${KUBECTL_NAMESPACE}" ]]; then
+  print '${KUBECTL_NAMESPACE} not defined. Try: source ./run/env.source';
+  exit 1
+fi
+
 for file in yaml/*.yaml; do
   cat "${file}" | \
     sed 's/\${KUBECTL_NAMESPACE}'"/${KUBECTL_NAMESPACE}/g" | \
